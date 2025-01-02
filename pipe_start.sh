bcbc0278762e355f5cbc14b648c76ca07567ddce
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2025-01-03 00:47:59 +0800

    Update pipe_start.sh

--- pipe_start.sh
+++ pipe_start.sh
@@ -1,23 +1,20 @@
 sudo apt-get update
 apt install git
 
-curl -s https://raw.githubusercontent.com/jiangyaqiii/envirment/main/python3_11.sh |bash
+apt install -y python3-pip
+pip install aiohttp
 
 git clone https://github.com/jiangyaqiii/pipe_detail.git
 cd pipe_detail 
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 # USER_TOKEN
 # USER_EMAIL
 # 将 token 和邮箱保存到 token.txt 文件中
-echo "$USER_TOKEN,$USER_EMAIL" > token.txt
+echo "$token,$email" > token.txt
 
 if tmux ls | grep -q pipe; then
     tmux kill-session -t pipe
 fi
 
-tmux new-session -d -s pipe  # 创建新的 tmux 会话，名称为 pipe
-tmux send-keys -t pipe "cd pipe" C-m  # 切换到 pipe 目录
-tmux send-keys -t pipe "python3 main.py" C-m  # 启动 main.py
-
-# 查看日志
-# tmux attach -t pipe
+cd pipe
+python main.py
